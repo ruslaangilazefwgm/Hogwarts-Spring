a5db9f8a8236238c3553e32a4b2b3f4d6ecab8fe
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-12-09 19:18:09 +0300

    1 урок

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,41 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.*;
+
+@Service
+public class FacultyService {
+    private final Map<Long, Faculty> faculties = new HashMap<>();
+    private long lastId = 0;
+
+    public Faculty createFaculty(Faculty faculty) {
+        faculty.setId(++lastId);
+        faculties.put(lastId, faculty);
+        return faculty;
+    }
+
+    public Faculty findFaculty(long id) {
+        return faculties.get(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        faculties.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty deleteFaculty(long id) {
+        return faculties.remove(id);
+    }
+
+    public Collection<Faculty> findByColor(String colour) {
+        ArrayList<Faculty> result = new ArrayList<>();
+        for (Faculty faculty : faculties.values()) {
+            if (Objects.equals(faculty.getColour(), colour)) {
+                result.add(faculty);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,42 @@
+package ru.hogwarts.school.service;
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+@Service
+public class StudentService {
+    private final Map<Long, Student> students = new HashMap<>();
+    private long lastId = 0;
+
+    public Student createStudent(Student student) {
+        student.setId(++lastId);
+        students.put(lastId, student);
+        return student;
+    }
+
+    public Student findStudent(long id) {
+        return students.get(id);
+    }
+
+    public Student editStudent(Student student) {
+        students.put(student.getId(), student);
+        return student;
+    }
+
+    public Student deleteStudent(long id) {
+        return students.remove(id);
+    }
+
+    public Collection<Student> findAge(long age) {
+        ArrayList<Student> result = new ArrayList<>();
+        for (Student student : students.values()) {
+            if (student.getAge() == age) {
+                result.add(student);
+            }
+        }
+        return result;
+    }
+}
